a489a7978f100f6e0eb66040b2d304d794ef75f2
xeusteerapat <xeus085@gmail.com>
2021-05-02 15:43:23 +0700

    [EDIT] update deploy getTodos

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,7 +1,32 @@
+// import * as AWS from 'aws-sdk'
 import 'source-map-support/register'
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  APIGatewayProxyHandler
+} from 'aws-lambda'
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+// const docClient = new AWS.DynamoDB.DocumentClient()
+// console.log(docClient)
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+// const todosTable = process.env.TODOS_TABLE
+// console.log(todosTable)
+
+export const handler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
   // TODO: Get all TODO items for a current user
+  console.log(`Processing event in GetTodos: ${event}`)
+
+  const todos = ['test']
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      items: todos
+    })
+  }
 }
