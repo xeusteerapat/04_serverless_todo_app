fa7e878f4988cbcb300eeb52a8bcc8615d6c50ba
xeusteerapat <xeus085@gmail.com>
2021-05-10 17:31:28 +0700

    [EDIT] delete todo

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -1,17 +1,41 @@
 import 'source-map-support/register'
-
 import {
   APIGatewayProxyEvent,
   APIGatewayProxyResult,
   APIGatewayProxyHandler
 } from 'aws-lambda'
+import { createLogger } from '../../utils/logger'
+import { getUserId } from '../utils'
+import { deleteTodo } from '../../businessLogic/todo'
+
+const logger = createLogger('Delete-Todo')
 
 export const handler: APIGatewayProxyHandler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
+  logger.info(`Processing delete todo ${event}`)
   const todoId = event.pathParameters.todoId
-  console.log(todoId)
+
+  if (!todoId) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        error: 'Missing todo parameter'
+      })
+    }
+  }
 
   // TODO: Remove a TODO item by id
-  return undefined
+  const userId = getUserId(event)
+  logger.info(`Deleting todo for user: ${{ userId, todoId }}`)
+
+  await deleteTodo(todoId, userId)
+
+  return {
+    statusCode: 204,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({})
+  }
 }
