95a6b8270e764f3d37a7768aadb42a244fd32049
xeusteerapat <xeus085@gmail.com>
2021-05-10 18:12:35 +0700

    [EDIT] update generate image url handler

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -1,17 +1,50 @@
 import 'source-map-support/register'
-
 import {
   APIGatewayProxyEvent,
   APIGatewayProxyResult,
   APIGatewayProxyHandler
 } from 'aws-lambda'
+import { createLogger } from '../../utils/logger'
+import { getUserId } from '../utils'
+import {
+  getImageSignedUrl,
+  updateAttachmentUrl
+} from '../../businessLogic/todo'
+
+const logger = createLogger('Generate-Upload-Url')
 
 export const handler: APIGatewayProxyHandler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
-  const todoId = event.pathParameters.todoId
-  console.log(todoId)
-
   // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  return undefined
+  logger.info(`Processing upload url ${event}`)
+
+  const todoId = event.pathParameters.todoId
+
+  if (!todoId) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        error: 'Missing TodoId parameter'
+      })
+    }
+  }
+
+  const userId = getUserId(event)
+  logger.info(`Get Image Signed URL for user: ${userId}`)
+
+  const signedUrl: string = await getImageSignedUrl(todoId)
+  logger.info(`Retreived signe url image ${signedUrl}`)
+
+  await updateAttachmentUrl(signedUrl, todoId, userId)
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      uploadUrl: signedUrl
+    })
+  }
 }
